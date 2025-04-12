3317a8affe0099a15d60011ce82e0d073c044ad8
Plain <hanjie-chen@outlook.com>
2025-04-12 12:20:57 +0000

    change something in network settings

--- README.md
+++ README.md
@@ -8,5 +8,19 @@ reference https://learn.microsoft.com/en-us/azure/virtual-machines/linux/quick-c
 
 # furture consider
 1/ use terraform moudle to refactor the existing code
-2/ for vm created, try to build enviroment
+2/ for vm created, try to build enviroment, for example in windows install vscode, chrome, windows termianl .. etc
+in linux install docker, python (virtual env), terraform ... etc
 3/ for terraform.tfstate can't store in the local, if it the file loss, the terraform will not regonize the Azure env.
+4/ when use terraform build the windows virutal machine change rdp port to another port not use 3389 port.\
+5/ optimization the comment, make the content more clear
+
+# how to use the repo
+在 terraform apply 之前需要创建一个 terraform.tfvars 并且在其中定义：
+
+```hcl
+subscription_id = "XXX"
+
+linux_vm_admin_password = "XXX"
+
+windows_vm_admin_password = "XXX"
+```
--- network-linux.tf
+++ network-linux.tf
@@ -36,17 +36,43 @@ resource "azurerm_network_security_group" "linux_subnet_nsg" {
     destination_address_prefix = var.linux_subnet[0]
   }
   # allow inbound 443 for proxy
+  # security_rule {
+  #   name                       = "AllowAnyCustom443Inbound"
+  #   priority                   = 110
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "*"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "443"
+  #   source_address_prefix      = "*"
+  #   destination_address_prefix = var.linux_subnet[0]
+  # }
+
+  # allow inbound 20000~50000 for proxy
   security_rule {
-    name                       = "AllowAnyCustom443Inbound"
+    name                       = "AllowProxyInbound"
     priority                   = 110
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "*"
+    protocol                   = "Udp"
     source_port_range          = "*"
-    destination_port_range     = "443"
+    destination_port_range     = "20000-50000"
     source_address_prefix      = "*"
     destination_address_prefix = var.linux_subnet[0]
   }
+
+  # allow inbound 80 for AFD access
+  # security_rule {
+  #   name                       = "AllowAnyCustom80Inbound"
+  #   priority                   = 120
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "80"
+  #   source_address_prefix      = "*"
+  #   destination_address_prefix = var.linux_subnet[0]
+  # }
 }
 
 # create linux nic
